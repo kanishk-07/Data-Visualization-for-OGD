65c14ebe57d38d70c11552a952d67404b183e1d0
Kanishk Sharma <kanishksharma809@gmail.com>
2022-06-02 04:32:56 +0530

    Update views.py

--- WaterBornDisease/views.py
+++ WaterBornDisease/views.py
@@ -3,44 +3,44 @@ from collections import OrderedDict
 from fusioncharts import FusionCharts
 import commonFunctions
 
-serviceUrl = ''
+serviceUrl = 'https://api.data.gov.in/resource/1fa8f9d2-c88e-4908-81b9-f6edca4b7799?api-key=<< api key >>&format=json'
 
 def WaterBornDisease(request):
-    #rawData = commonFunctions.downloadData(serviceUrl)
-    #js = commonFunctions.loadJson(rawData)
-    #data = js['records']
-    js = {"records": [
-        {"states_uts": "Andaman & Nicobar Islands", "Expenditure": "0.43", "Cases": "15.357", "projects": "16"},
-        {"states_uts": "Andhra Pradesh", "Expenditure": "159.51", "Cases": "1163.707", "projects": "16"},
-        {"states_uts": "Arunachal Pradesh", "Expenditure": "77.51", "Cases": "23.888", "projects": "1"},
-        {"states_uts": "Assam", "Expenditure": "524.1", "Cases": "165.347", "projects": "11"},
-        {"states_uts": "Bihar", "Expenditure": "394.53", "Cases": "309.289", "projects": "15"},
-        {"states_uts": "Chhattisgarh", "Expenditure": "64.33", "Cases": "180.398", "projects": "7"},
-        {"states_uts": "Goa", "Expenditure": "2.32", "Cases": "20.706", "projects": "1"},
-        {"states_uts": "Gujarat", "Expenditure": "231.62", "Cases": "667.459", "projects": "27"},
-        {"states_uts": "Haryana", "Expenditure": "118.95", "Cases": "236.752", "projects": "2"},
-        {"states_uts": "Himachal Pradesh", "Expenditure": "66.02", "Cases": "314.463", "projects": "36"},
-        {"states_uts": "Jammu & Kashmir", "Expenditure": "222.26", "Cases": "512.376", "projects": "2"},
-        {"states_uts": "Jharkhand", "Expenditure": "172.68", "Cases": "91.326", "projects": "10"},
-        {"states_uts": "Karnataka", "Expenditure": "290.86", "Cases": "917.488", "projects": "57"},
-        {"states_uts": "Kerala", "Expenditure": "56.88", "Cases": "452.859", "projects": "15"},
-        {"states_uts": "Madhya Pradesh", "Expenditure": "195.67", "Cases": "698.396", "projects": "20"},
-        {"states_uts": "Maharashtra", "Expenditure": "338.13", "Cases": "611.920", "projects": "102"},
-        {"states_uts": "Manipur", "Expenditure": "32.2", "Cases": "32.085", "projects": "3"},
-        {"states_uts": "Meghalaya", "Expenditure": "41.86", "Cases": "124.461", "projects": "2"},
-        {"states_uts": "Mizoram", "Expenditure": "22.4", "Cases": "15.885", "projects": "2"},
-        {"states_uts": "Nagaland", "Expenditure": "27.66", "Cases": "14.638", "projects": "2"},
-        {"states_uts": "Odisha", "Expenditure": "102.69", "Cases": "626.421", "projects": "5"},
-        {"states_uts": "Punjab", "Expenditure": "115.25", "Cases": "203.510", "projects": "13"},
-        {"states_uts": "Rajasthan", "Expenditure": "891.95", "Cases": "971.113", "projects": "34"},
-        {"states_uts": "Sikkim", "Expenditure": "9.3", "Cases": "39.624", "projects": "2"},
-        {"states_uts": "Tamil Nadu", "Expenditure": "121.67", "Cases": "407.678", "projects": "53"},
-        {"states_uts": "Telangana", "Expenditure": "775.01", "Cases": "487.099", "projects": "11"},
-        {"states_uts": "Tripura", "Expenditure": "32.26", "Cases": "77.434", "projects": "2"},
-        {"states_uts": "Uttar Pradesh", "Expenditure": "539.09", "Cases": "1219.071", "projects": "11"},
-        {"states_uts": "Uttarakhand", "Expenditure": "68.42", "Cases": "98.780", "projects": "16"},
-        {"states_uts": "West Bengal", "Expenditure": "810.48", "Cases": "1888.794", "projects": "10"}]}
-    data = js["records"]
+    rawData = commonFunctions.downloadData(serviceUrl)
+    js = commonFunctions.loadJson(rawData)
+    data = js['records']
+    #js = {"records": [
+    #    {"states_uts": "Andaman & Nicobar Islands", "Expenditure": "0.43", "Cases": "15.357", "projects": "16"},
+    #    {"states_uts": "Andhra Pradesh", "Expenditure": "159.51", "Cases": "1163.707", "projects": "16"},
+    #    {"states_uts": "Arunachal Pradesh", "Expenditure": "77.51", "Cases": "23.888", "projects": "1"},
+    #    {"states_uts": "Assam", "Expenditure": "524.1", "Cases": "165.347", "projects": "11"},
+    #    {"states_uts": "Bihar", "Expenditure": "394.53", "Cases": "309.289", "projects": "15"},
+    #    {"states_uts": "Chhattisgarh", "Expenditure": "64.33", "Cases": "180.398", "projects": "7"},
+    #    {"states_uts": "Goa", "Expenditure": "2.32", "Cases": "20.706", "projects": "1"},
+    #    {"states_uts": "Gujarat", "Expenditure": "231.62", "Cases": "667.459", "projects": "27"},
+    #    {"states_uts": "Haryana", "Expenditure": "118.95", "Cases": "236.752", "projects": "2"},
+    #    {"states_uts": "Himachal Pradesh", "Expenditure": "66.02", "Cases": "314.463", "projects": "36"},
+    #    {"states_uts": "Jammu & Kashmir", "Expenditure": "222.26", "Cases": "512.376", "projects": "2"},
+    #    {"states_uts": "Jharkhand", "Expenditure": "172.68", "Cases": "91.326", "projects": "10"},
+    #    {"states_uts": "Karnataka", "Expenditure": "290.86", "Cases": "917.488", "projects": "57"},
+    #    {"states_uts": "Kerala", "Expenditure": "56.88", "Cases": "452.859", "projects": "15"},
+    #    {"states_uts": "Madhya Pradesh", "Expenditure": "195.67", "Cases": "698.396", "projects": "20"},
+    #    {"states_uts": "Maharashtra", "Expenditure": "338.13", "Cases": "611.920", "projects": "102"},
+    #    {"states_uts": "Manipur", "Expenditure": "32.2", "Cases": "32.085", "projects": "3"},
+    #    {"states_uts": "Meghalaya", "Expenditure": "41.86", "Cases": "124.461", "projects": "2"},
+    #    {"states_uts": "Mizoram", "Expenditure": "22.4", "Cases": "15.885", "projects": "2"},
+    #    {"states_uts": "Nagaland", "Expenditure": "27.66", "Cases": "14.638", "projects": "2"},
+    #    {"states_uts": "Odisha", "Expenditure": "102.69", "Cases": "626.421", "projects": "5"},
+    #    {"states_uts": "Punjab", "Expenditure": "115.25", "Cases": "203.510", "projects": "13"},
+    #    {"states_uts": "Rajasthan", "Expenditure": "891.95", "Cases": "971.113", "projects": "34"},
+    #    {"states_uts": "Sikkim", "Expenditure": "9.3", "Cases": "39.624", "projects": "2"},
+    #    {"states_uts": "Tamil Nadu", "Expenditure": "121.67", "Cases": "407.678", "projects": "53"},
+    #    {"states_uts": "Telangana", "Expenditure": "775.01", "Cases": "487.099", "projects": "11"},
+    #    {"states_uts": "Tripura", "Expenditure": "32.26", "Cases": "77.434", "projects": "2"},
+    #    {"states_uts": "Uttar Pradesh", "Expenditure": "539.09", "Cases": "1219.071", "projects": "11"},
+    #    {"states_uts": "Uttarakhand", "Expenditure": "68.42", "Cases": "98.780", "projects": "16"},
+    #    {"states_uts": "West Bengal", "Expenditure": "810.48", "Cases": "1888.794", "projects": "10"}]}
+    #data = js["records"]
 
     # *****************************Chloropleth Map*****************************#
     mapConfig = OrderedDict()
