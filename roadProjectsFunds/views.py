b69136a9332502fc8593fa1ffa52ba53c9b56efe
Kanishk Sharma <kanishksharma809@gmail.com>
2022-06-02 04:39:25 +0530

    Update views.py

--- roadProjectsFunds/views.py
+++ roadProjectsFunds/views.py
@@ -9,38 +9,38 @@ urlLaneWiseLenNH = "https://api.data.gov.in/resource/54864dd4-9e8d-4935-a6fe-aed
 
 def roadProjectsFunds(request):
     rawData = commonFunctions.downloadData(urlFunds)
-    #js = commonFunctions.loadJson(rawData)
-    #data = js['records']
-    js = {"records": [{"states_uts": "Andhra Pradesh", "acq2016-2017": "348.72", "acq2017-2018": "316.45", "projects":"16"},
-                        {"states_uts": "Arunachal Pradesh", "acq2016-2017": "131.11", "acq2017-2018": "122.47", "projects":"1"},
-                        {"states_uts": "Assam", "acq2016-2017": "144.08", "acq2017-2018": "134.77", "projects":"11"},
-                        {"states_uts": "Bihar", "acq2016-2017": "200.46", "acq2017-2018": "190.11", "projects":"15"},
-                        {"states_uts": "Chhattisgarh", "acq2016-2017": "247.3", "acq2017-2018": "231.16", "projects":"7"},
-                        {"states_uts": "Goa", "acq2016-2017": "17.08", "acq2017-2018": "16.49", "projects":"1"},
-                        {"states_uts": "Gujarat", "acq2016-2017": "445.09", "acq2017-2018": "412.84", "projects":"27"},
-                        {"states_uts": "Haryana", "acq2016-2017": "194.74", "acq2017-2018": "180.11", "projects":"2"},
-                        {"states_uts": "Himachal Pradesh", "acq2016-2017": "100.91", "acq2017-2018": "94.85", "projects":"36"},
-                        {"states_uts": "Jammu & Kashmir", "acq2016-2017": "357.32", "acq2017-2018": "332.57", "projects":"2"},
-                        {"states_uts": "Jharkhand", "acq2016-2017": "164.63", "acq2017-2018": "153.23", "projects":"10"},
-                        {"states_uts": "Karnataka", "acq2016-2017": "450.95", "acq2017-2018": "430", "projects":"57"},
-                        {"states_uts": "Kerala", "acq2016-2017": "150.37", "acq2017-2018": "132.69", "projects":"15"},
-                        {"states_uts": "Madhya Pradesh", "acq2016-2017": "566.39", "acq2017-2018": "525.83", "projects":"20"},
-                        {"states_uts": "Maharashtra", "acq2016-2017": "717.66", "acq2017-2018": "660.16", "projects":"102"},
-                        {"states_uts": "Manipur", "acq2016-2017": "37.48", "acq2017-2018": "34.19", "projects":"3"},
-                        {"states_uts": "Meghalaya", "acq2016-2017": "42.57", "acq2017-2018": "39.05", "projects":"2"},
-                        {"states_uts": "Mizoram", "acq2016-2017": "34.18", "acq2017-2018": "31.7", "projects":"2"},
-                        {"states_uts": "Nagaland", "acq2016-2017": "27.42", "acq2017-2018": "29.17", "projects":"2"},
-                        {"states_uts": "Odisha", "acq2016-2017": "295.71", "acq2017-2018": "279.93", "projects":"5"},
-                        {"states_uts": "Punjab", "acq2016-2017": "167.04", "acq2017-2018": "156.63", "projects":"13"},
-                        {"states_uts": "Rajasthan", "acq2016-2017": "663.06", "acq2017-2018": "617.93", "projects":"34"},
-                        {"states_uts": "Sikkim", "acq2016-2017": "12.63", "acq2017-2018": "11.64", "projects":"2"},
-                        {"states_uts": "Tamil Nadu", "acq2016-2017": "392.02", "acq2017-2018": "361.19", "projects":"53"},
-                        {"states_uts": "Telangana", "acq2016-2017": "258.81", "acq2017-2018": "249.13", "projects":"11"},
-                        {"states_uts": "Tripura", "acq2016-2017": "19.09", "acq2017-2018": "17.54", "projects":"2"},
-                        {"states_uts": "Uttar Pradesh", "acq2016-2017": "569.23", "acq2017-2018": "587.07", "projects":"11"},
-                        {"states_uts": "Uttarakhand", "acq2016-2017": "103.68", "acq2017-2018": "96.16", "projects":"16"},
-                        {"states_uts": "West Bengal", "acq2016-2017": "210.97", "acq2017-2018": "201.66", "projects":"10"} ]}
-    data = js["records"]
+    js = commonFunctions.loadJson(rawData)
+    data = js['records']
+    #js = {"records": [{"states_uts": "Andhra Pradesh", "acq2016-2017": "348.72", "acq2017-2018": "316.45", "projects":"16"},
+    #                    {"states_uts": "Arunachal Pradesh", "acq2016-2017": "131.11", "acq2017-2018": "122.47", "projects":"1"},
+    #                    {"states_uts": "Assam", "acq2016-2017": "144.08", "acq2017-2018": "134.77", "projects":"11"},
+    #                    {"states_uts": "Bihar", "acq2016-2017": "200.46", "acq2017-2018": "190.11", "projects":"15"},
+    #                    {"states_uts": "Chhattisgarh", "acq2016-2017": "247.3", "acq2017-2018": "231.16", "projects":"7"},
+    #                    {"states_uts": "Goa", "acq2016-2017": "17.08", "acq2017-2018": "16.49", "projects":"1"},
+    #                    {"states_uts": "Gujarat", "acq2016-2017": "445.09", "acq2017-2018": "412.84", "projects":"27"},
+    #                    {"states_uts": "Haryana", "acq2016-2017": "194.74", "acq2017-2018": "180.11", "projects":"2"},
+    #                    {"states_uts": "Himachal Pradesh", "acq2016-2017": "100.91", "acq2017-2018": "94.85", "projects":"36"},
+    #                    {"states_uts": "Jammu & Kashmir", "acq2016-2017": "357.32", "acq2017-2018": "332.57", "projects":"2"},
+    #                    {"states_uts": "Jharkhand", "acq2016-2017": "164.63", "acq2017-2018": "153.23", "projects":"10"},
+    #                    {"states_uts": "Karnataka", "acq2016-2017": "450.95", "acq2017-2018": "430", "projects":"57"},
+    #                    {"states_uts": "Kerala", "acq2016-2017": "150.37", "acq2017-2018": "132.69", "projects":"15"},
+    #                    {"states_uts": "Madhya Pradesh", "acq2016-2017": "566.39", "acq2017-2018": "525.83", "projects":"20"},
+    #                    {"states_uts": "Maharashtra", "acq2016-2017": "717.66", "acq2017-2018": "660.16", "projects":"102"},
+    #                    {"states_uts": "Manipur", "acq2016-2017": "37.48", "acq2017-2018": "34.19", "projects":"3"},
+    #                    {"states_uts": "Meghalaya", "acq2016-2017": "42.57", "acq2017-2018": "39.05", "projects":"2"},
+    #                    {"states_uts": "Mizoram", "acq2016-2017": "34.18", "acq2017-2018": "31.7", "projects":"2"},
+    #                    {"states_uts": "Nagaland", "acq2016-2017": "27.42", "acq2017-2018": "29.17", "projects":"2"},
+    #                    {"states_uts": "Odisha", "acq2016-2017": "295.71", "acq2017-2018": "279.93", "projects":"5"},
+    #                    {"states_uts": "Punjab", "acq2016-2017": "167.04", "acq2017-2018": "156.63", "projects":"13"},
+    #                    {"states_uts": "Rajasthan", "acq2016-2017": "663.06", "acq2017-2018": "617.93", "projects":"34"},
+    #                    {"states_uts": "Sikkim", "acq2016-2017": "12.63", "acq2017-2018": "11.64", "projects":"2"},
+    #                    {"states_uts": "Tamil Nadu", "acq2016-2017": "392.02", "acq2017-2018": "361.19", "projects":"53"},
+    #                    {"states_uts": "Telangana", "acq2016-2017": "258.81", "acq2017-2018": "249.13", "projects":"11"},
+    #                    {"states_uts": "Tripura", "acq2016-2017": "19.09", "acq2017-2018": "17.54", "projects":"2"},
+    #                    {"states_uts": "Uttar Pradesh", "acq2016-2017": "569.23", "acq2017-2018": "587.07", "projects":"11"},
+    #                    {"states_uts": "Uttarakhand", "acq2016-2017": "103.68", "acq2017-2018": "96.16", "projects":"16"},
+    #                    {"states_uts": "West Bengal", "acq2016-2017": "210.97", "acq2017-2018": "201.66", "projects":"10"} ]}
+    #data = js["records"]
     # *****************************Choropleth Map*****************************#
 
     mapConfig = OrderedDict()
@@ -254,116 +254,116 @@ def roadProjectsFunds(request):
     ############
     ###########
 
-    # rawData = commonFunctions.downloadData(urlLaneWiseLenNH)
-    # js = commonFunctions.loadJson(rawData)
-    # data = js['records']
-    #
-    # for i in range(29):
-    #     stateName = data[i]["states_uts"]
-    #     if stateName == "Jammu and Kashmir":
-    #         stateName = "Jammu & Kashmir"
-    #     commonFunctions.stateDictionary[stateName][0] = float(data[i]["total_length"])
-    #     commonFunctions.stateDictionary[stateName][1] = float(data[i]["less_than_2_lane"])
-    #     commonFunctions.stateDictionary[stateName][2] = float(data[i]["two_lane"])
-    #     commonFunctions.stateDictionary[stateName][3] = float(data[i]["four_lane_above"])
-    #
-    # mapDataArrayTotalLength = [
-    #     ["002", str(commonFunctions.stateDictionary["Andhra Pradesh"][0]), "1"],  # AP
-    #     ["003", str(commonFunctions.stateDictionary["Arunachal Pradesh"][0]), "1"],  # AR
-    #     ["004", str(commonFunctions.stateDictionary["Assam"][0]), "1"], # AS
-    #     ["005", str(commonFunctions.stateDictionary["Bihar"][0]), "1"],
-    #     ["007", str(commonFunctions.stateDictionary["Chhattisgarh"][0]), "1"],  # CA
-    #     ["011", str(commonFunctions.stateDictionary["Goa"][0]), "1"],  # GO
-    #     ["012", str(commonFunctions.stateDictionary["Gujarat"][0]), "1"],  # GU
-    #     ["013", str(commonFunctions.stateDictionary["Haryana"][0]), "1"],  # HA
-    #     ["014", str(commonFunctions.stateDictionary["Himachal Pradesh"][0]), "1"],  # HP
-    #     ["015", str(commonFunctions.stateDictionary["Jammu & Kashmir"][0]), "1"],  # JK
-    #     ["016", str(commonFunctions.stateDictionary["Jharkhand"][0]), "1"],  # JH
-    #     ["017", str(commonFunctions.stateDictionary["Karnataka"][0]), "1"],  # KA
-    #     ["018", str(commonFunctions.stateDictionary["Kerala"][0]), "1"], # KE
-    #     ["020", str(commonFunctions.stateDictionary["Madhya Pradesh"][0]), "1"],  # MP
-    #     ["021", str(commonFunctions.stateDictionary["Maharashtra"][0]), "1"],  # MA
-    #     ["022", str(commonFunctions.stateDictionary["Manipur"][0]), "1"],  # MN
-    #     ["023", str(commonFunctions.stateDictionary["Meghalaya"][0]), "1"],  # ME
-    #     ["024", str(commonFunctions.stateDictionary["Mizoram"][0]), "1"],  # MI
-    #     ["025", str(commonFunctions.stateDictionary["Nagaland"][0]), "1"],  # NA
-    #     ["026", str(commonFunctions.stateDictionary["Odisha"][0]), "1"], # OR
-    #     ["028", str(commonFunctions.stateDictionary["Punjab"][0]), "1"], # PU
-    #     ["029", str(commonFunctions.stateDictionary["Rajasthan"][0]), "1"],  # RA
-    #     ["030", str(commonFunctions.stateDictionary["Sikkim"][0]), "1"], # SI
-    #     ["031", str(commonFunctions.stateDictionary["Tamil Nadu"][0]), "1"], # TN
-    #     ["032", str(commonFunctions.stateDictionary["Tripura"][0]), "1"],  # TR
-    #     ["036", str(commonFunctions.stateDictionary["Telangana"][0]), "1"],  # TG
-    #     ["033", str(commonFunctions.stateDictionary["Uttar Pradesh"][0]), "1"],  # UP
-    #     ["034", str(commonFunctions.stateDictionary["Uttarakhand"][0]), "1"],  # UT
-    #     ["035", str(commonFunctions.stateDictionary["West Bengal"][0]), "1"]  # WB
-    # ]
-    #
-    # colorDataObj = {
-    #     "minvalue": "0",
-    #     "code": "#FFE0B2",
-    #     "gradient": "1",
-    #     "color": [{
-    #         "minValue": "0",
-    #         "maxValue": "260",
-    #         "code": "#f4fdff"
-    #     },
-    #         {
-    #             "minValue": "260",
-    #             "maxValue": "1500",
-    #             "code": "#c0dae0"
-    #         },
-    #         {
-    #             "minValue": "1500",
-    #             "maxValue": "3000",
-    #             "code": "#8bc8d6"
-    #         },
-    #         {
-    #             "minValue": "3000",
-    #             "maxValue": "4500",
-    #             "code": "#57b6cc"
-    #         },
-    #         {
-    #             "minValue": "4500",
-    #             "maxValue": "6000",
-    #             "code": "#037db7"
-    #         },
-    #         {
-    #             "minValue": "6000",
-    #             "maxValue": "7500",
-    #             "code": "#0365b7"
-    #         },
-    #         {
-    #             "minValue": "7500",
-    #             "maxValue": "9000",
-    #             "code": "#033fb7"
-    #         }
-    #     ]
-    # }
-    #
-    # mapConfig1 = OrderedDict()
-    # mapConfig1["animation"] = "0"
-    # mapConfig1["usehovercolor"] = "1"
-    # mapConfig1["showlegend"] = "1"
-    # mapConfig1["legendposition"] = "BOTTOM"
-    # mapConfig1["legendborderalpha"] = "0"
-    # mapConfig1["legendbordercolor"] = "#FDFDB7"
-    # mapConfig1["legendallowdrag"] = "0"
-    # mapConfig1["legendshadow"] = "0"
-    # mapConfig1["numbersuffix"] = " km"
-    # mapConfig1["caption"] = "Data of March 2015"
-    # mapConfig1["subcaption"] = ""
-    # mapConfig1["connectorcolor"] = "#FDFDB7"
-    # mapConfig1["fillalpha"] = "80"
-    # mapConfig1["entityFillHoverColor"] = "#dddddd"
-    # mapConfig1["theme"] = "fusion"
-    #
-    # dataSourceRoadLength = OrderedDict()
-    # dataSourceRoadLength["chart"] = mapConfig1
-    # dataSourceRoadLength["colorrange"] = colorDataObj
-    # dataSourceRoadLength["data"] = commonFunctions.makeDataSourceForChloroplethMap(mapDataArrayTotalLength)
-    #
-    # fusionMapRoadLength = FusionCharts("maps/india", "Map3", "600", "600", "RoadLengthStates", "json", dataSourceRoadLength)
+    rawData = commonFunctions.downloadData(urlLaneWiseLenNH)
+    js = commonFunctions.loadJson(rawData)
+    data = js['records']
+    
+    for i in range(29):
+        stateName = data[i]["states_uts"]
+        if stateName == "Jammu and Kashmir":
+            stateName = "Jammu & Kashmir"
+        commonFunctions.stateDictionary[stateName][0] = float(data[i]["total_length"])
+        commonFunctions.stateDictionary[stateName][1] = float(data[i]["less_than_2_lane"])
+        commonFunctions.stateDictionary[stateName][2] = float(data[i]["two_lane"])
+        commonFunctions.stateDictionary[stateName][3] = float(data[i]["four_lane_above"])
+    
+     mapDataArrayTotalLength = [
+         ["002", str(commonFunctions.stateDictionary["Andhra Pradesh"][0]), "1"],  # AP
+         ["003", str(commonFunctions.stateDictionary["Arunachal Pradesh"][0]), "1"],  # AR
+         ["004", str(commonFunctions.stateDictionary["Assam"][0]), "1"], # AS
+         ["005", str(commonFunctions.stateDictionary["Bihar"][0]), "1"],
+         ["007", str(commonFunctions.stateDictionary["Chhattisgarh"][0]), "1"],  # CA
+         ["011", str(commonFunctions.stateDictionary["Goa"][0]), "1"],  # GO
+         ["012", str(commonFunctions.stateDictionary["Gujarat"][0]), "1"],  # GU
+         ["013", str(commonFunctions.stateDictionary["Haryana"][0]), "1"],  # HA
+         ["014", str(commonFunctions.stateDictionary["Himachal Pradesh"][0]), "1"],  # HP
+         ["015", str(commonFunctions.stateDictionary["Jammu & Kashmir"][0]), "1"],  # JK
+         ["016", str(commonFunctions.stateDictionary["Jharkhand"][0]), "1"],  # JH
+         ["017", str(commonFunctions.stateDictionary["Karnataka"][0]), "1"],  # KA
+         ["018", str(commonFunctions.stateDictionary["Kerala"][0]), "1"], # KE
+         ["020", str(commonFunctions.stateDictionary["Madhya Pradesh"][0]), "1"],  # MP
+         ["021", str(commonFunctions.stateDictionary["Maharashtra"][0]), "1"],  # MA
+         ["022", str(commonFunctions.stateDictionary["Manipur"][0]), "1"],  # MN
+         ["023", str(commonFunctions.stateDictionary["Meghalaya"][0]), "1"],  # ME
+         ["024", str(commonFunctions.stateDictionary["Mizoram"][0]), "1"],  # MI
+         ["025", str(commonFunctions.stateDictionary["Nagaland"][0]), "1"],  # NA
+         ["026", str(commonFunctions.stateDictionary["Odisha"][0]), "1"], # OR
+         ["028", str(commonFunctions.stateDictionary["Punjab"][0]), "1"], # PU
+         ["029", str(commonFunctions.stateDictionary["Rajasthan"][0]), "1"],  # RA
+         ["030", str(commonFunctions.stateDictionary["Sikkim"][0]), "1"], # SI
+         ["031", str(commonFunctions.stateDictionary["Tamil Nadu"][0]), "1"], # TN
+         ["032", str(commonFunctions.stateDictionary["Tripura"][0]), "1"],  # TR
+         ["036", str(commonFunctions.stateDictionary["Telangana"][0]), "1"],  # TG
+         ["033", str(commonFunctions.stateDictionary["Uttar Pradesh"][0]), "1"],  # UP
+         ["034", str(commonFunctions.stateDictionary["Uttarakhand"][0]), "1"],  # UT
+         ["035", str(commonFunctions.stateDictionary["West Bengal"][0]), "1"]  # WB
+     ]
+    
+     colorDataObj = {
+         "minvalue": "0",
+         "code": "#FFE0B2",
+         "gradient": "1",
+         "color": [{
+             "minValue": "0",
+             "maxValue": "260",
+             "code": "#f4fdff"
+         },
+             {
+                 "minValue": "260",
+                 "maxValue": "1500",
+                 "code": "#c0dae0"
+             },
+             {
+                 "minValue": "1500",
+                 "maxValue": "3000",
+                 "code": "#8bc8d6"
+             },
+             {
+                 "minValue": "3000",
+                 "maxValue": "4500",
+                 "code": "#57b6cc"
+             },
+             {
+                 "minValue": "4500",
+                 "maxValue": "6000",
+                 "code": "#037db7"
+             },
+             {
+                 "minValue": "6000",
+                 "maxValue": "7500",
+                 "code": "#0365b7"
+             },
+             {
+                 "minValue": "7500",
+                 "maxValue": "9000",
+                 "code": "#033fb7"
+             }
+         ]
+     }
+    
+     mapConfig1 = OrderedDict()
+     mapConfig1["animation"] = "0"
+     mapConfig1["usehovercolor"] = "1"
+     mapConfig1["showlegend"] = "1"
+     mapConfig1["legendposition"] = "BOTTOM"
+     mapConfig1["legendborderalpha"] = "0"
+     mapConfig1["legendbordercolor"] = "#FDFDB7"
+     mapConfig1["legendallowdrag"] = "0"
+     mapConfig1["legendshadow"] = "0"
+     mapConfig1["numbersuffix"] = " km"
+     mapConfig1["caption"] = "Data of March 2015"
+     mapConfig1["subcaption"] = ""
+     mapConfig1["connectorcolor"] = "#FDFDB7"
+     mapConfig1["fillalpha"] = "80"
+     mapConfig1["entityFillHoverColor"] = "#dddddd"
+     mapConfig1["theme"] = "fusion"
+    
+    dataSourceRoadLength = OrderedDict()
+    dataSourceRoadLength["chart"] = mapConfig1
+    dataSourceRoadLength["colorrange"] = colorDataObj
+    dataSourceRoadLength["data"] = commonFunctions.makeDataSourceForChloroplethMap(mapDataArrayTotalLength)
+    
+    fusionMapRoadLength = FusionCharts("maps/india", "Map3", "600", "600", "RoadLengthStates", "json", dataSourceRoadLength)
 
     # ********************************************************************#
 
